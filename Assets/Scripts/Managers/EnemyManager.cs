753ed3b86732c87273756e7a306736e26778d515
Andy Hendriks <andy@live.nl>
2024-10-07 16:22:25 +0200

    Enemy + Notification UI

--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyManager : MonoBehaviour
+public class EnemySpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject enemyPrefab;  // The enemy prefab to spawn
+    public List<Transform> spawnPoints;  // List of spawn points
+    public float spawnInterval = 5f; // Time between spawns
+
+    private void Start()
     {
-        
+        StartCoroutine(SpawnEnemies());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator SpawnEnemies()
     {
-        
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnInterval);
+            SpawnRandomEnemy();
+        }
+    }
+
+    private void SpawnRandomEnemy()
+    {
+        if (spawnPoints.Count == 0)
+        {
+            Debug.LogWarning("No spawn points set!");
+            return;
+        }
+
+        // Pick a random spawn point
+        int randomIndex = Random.Range(0, spawnPoints.Count);
+        Transform spawnPoint = spawnPoints[randomIndex];
+
+        // Instantiate the enemy at the chosen spawn point
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
     }
 }
